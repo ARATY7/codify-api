d9124be5a9ffc852b7c5719165c7835b57534758
Noé Henchoz <nhenchoz122@icloud.com>
2023-11-27 12:23:40 +0000

    CORS fixed

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://codify.henchoz.org');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
   next();
